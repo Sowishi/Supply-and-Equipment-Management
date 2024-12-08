0ba64e048c785337c33f4740d1e2643adf55ea58
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2024-12-09 00:31:44 +0800

    Completed forms in master table

--- src/components/inspectionModal.jsx
+++ src/components/inspectionModal.jsx
@@ -91,9 +91,9 @@ const InspectionModal = ({ title, size, open, handleClose, data }) => {
         <div className="border flex border-slate-950 text-center">
           <div className="basis-full border p-10 border-slate-950 ">
             <h1 className="font-bold mb-5 uppercase">Inspection</h1>
-            <h1>Juan Dela Cruz</h1>
+            <h1>Alma C. Furuc</h1>
             <h1 className="border-t-2 border-slate-950">
-              Inspection Officer / Inspection Committee
+              Inspection Officer / Administrative Aide IV
             </h1>
           </div>
           {/* <div className="basis-6/12 border p-10 border-slate-950 ">
